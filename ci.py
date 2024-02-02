490196c1e63265df8f6806a07930cea74aa0d7a6
Andrey Eremin <eremin@slotlogic.eu>
2024-02-03 00:59:27 +0300

    1

--- ci.py
+++ ci.py
@@ -1,5 +1,5 @@
-# Check images by OpenCV
-# Version 1.2.1
+# Check images by OpenCV with GUI
+# Version 1.0
 
 import argparse
 import cv2
@@ -14,15 +14,14 @@ class InitServiceOutputInfo:
     to the console and to the file.
     """
 
-    def __init__(self, tmpls_dict: dict, path_to_tmpls: tuple, precision: float):
+    def __init__(self, tmpls: list, precision: float):
         """
         :param tmpls_dict:    dict, contains pairs - {path to template images: list of images, ...}
         :param path_to_tmpls: tuple, 2 items: 0 - str, path to one image file or folder with image files, folder
                               1 - str, filename if the input is one file, else - empty string
         :param precision:     float
         """
-        self.__tmpls_dict = tmpls_dict
-        self.__path_to_tmpls = path_to_tmpls
+        self.__tmpls = tmpls
         self.__prec = precision
 
         self.__img_indent = 0
@@ -31,48 +30,38 @@ class InitServiceOutputInfo:
         self.__coord_indent = 0
 
     def __count_indents(self):
-        length_list = []
-        if len(self.__path_to_tmpls[1]) > 0:
-            img_name = self.__path_to_tmpls[1]
-            length_list.append(len(img_name))
-        else:
-            for tmpl_path, tmpl_names_list in self.__tmpls_dict.items():
-                subdir_name = tmpl_path.split(self.__path_to_tmpls[0])[1]
-                length_list += list(map(lambda x: len(subdir_name) + len(x), tmpl_names_list))
+        files = (os.path.basename(filename) for filename in self.__tmpls)
+        max_length = max(map(len, files))
 
-        self.__img_indent = max(length_list) + 5
+        self.__img_indent = max_length + 5
         self.__count_indent = 8
         self.__threshold_indent = len(str(self.__prec)) + 6
         self.__coord_indent = 8
 
-    def __calculate_header_indents(self, max_length: int, string_length: int) -> (int, int):
+    @staticmethod
+    def __calculate_header_indents(max_length: int, string_length: int) -> (int, int):
         left_indent = (max_length - string_length) // 2
         right_indent = max_length - string_length - left_indent
         return left_indent, right_indent
 
-    def print_header(self, scr_image: str) -> (str, str, str, str, str):
-        tmpls_folder = f'|   FOLDER - {self.__path_to_tmpls[0]}   |'
+    def print_header(self, scr_image: str) -> (str, str, str, str):
         screenshot = f'|   SCREENSHOT - {scr_image}   |'
         precision = f'|   PRECISION - {self.__prec}   ///   '
         time = datetime.datetime.now()
         time_as_str = time.strftime('%Y-%m-%d %H:%M:%S') + '   |'
-        third_string = precision + time_as_str
+        second_string = precision + time_as_str
 
-        max_length = max(len(tmpls_folder), len(screenshot), len(third_string))
+        max_length = max(len(screenshot), len(second_string))
 
         # 1st string
-        left, right = self.__calculate_header_indents(max_length, len(tmpls_folder))
-        tmpls_folder = f'|   {" " * left}FOLDER - {self.__path_to_tmpls[0]}{" " * right}   |'
-
-        # 2nd string
         left, right = self.__calculate_header_indents(max_length, len(screenshot))
         screenshot = f'|   {" " * left}SCREENSHOT - {scr_image}{" " * right}   |'
 
-        # 3rd string
-        left, right = self.__calculate_header_indents(max_length, len(third_string))
+        # 2nd string
+        left, right = self.__calculate_header_indents(max_length, len(second_string))
         precision = f'|   {" " * left}PRECISION - {self.__prec}   ///   '
         time_as_str = time.strftime('%Y-%m-%d %H:%M:%S') + f'{" " * right}   |'
-        third_string = precision + time_as_str
+        second_string = precision + time_as_str
 
         self.__count_indents()
 
@@ -80,17 +69,16 @@ class InitServiceOutputInfo:
                  f'X{" " * (self.__coord_indent - 1)}|Y{" " * (self.__coord_indent - 1)}'
 
         print('-' * max_length,
-              tmpls_folder,
               screenshot,
-              third_string,
+              second_string,
               '-' * max_length,
               header,
-              '-' * max_length, sep='\n')
+              '-' * max_length,
+              sep='\n')
 
         return ('-' * max_length + '\n',
-                tmpls_folder + '\n',
                 screenshot + '\n',
-                third_string + '\n',
+                second_string + '\n',
                 header + '\n')
 
     @property
@@ -116,7 +104,7 @@ class OutputInfo:
     the console and to the file.
     """
 
-    def __init__(self, path_to_tmpls: str, thr: float, precision: float, tmpl_indent: int, count_indent: int,
+    def __init__(self, filename: str, thr: float, precision: float, tmpl_indent: int, count_indent: int,
                  thr_indent: int, coord_indent: int, tmpl=None, count=None, x=None, y=None):
         """
         :param path_to_tmpls: str, path to folder with image templates
@@ -131,7 +119,7 @@ class OutputInfo:
         :param x:             int, x coordinate, where template found on screen image
         :param y:             int, y coordinate, where template found on screen image
         """
-        self.__path_to_tmpls = path_to_tmpls
+        self.__path_to_tmpls = filename
         self.__thr = thr
         self.__prec = precision
         self.__tmpl_indent = tmpl_indent
@@ -182,9 +170,9 @@ class CheckImages:
 
     __extension_list = ['png', 'jpg', 'webp']
     __tmpls_dict = dict()   # {'D:\\Python\\Check Images\\tf': ['e.png', 't.png'], ...}
-    __scr_img_gray_list = list()
+    __l_scr_img_gray = list()
 
-    def __init__(self, path_to_tmpls: str, screen_img: str, min_threshold=0.6, precision=0.0001):
+    def __init__(self, tmpls: list, screen_imgs: list, min_threshold=0.6, precision=0.0001):
         """
         :param path_to_tmpls:      str, path to one image file or folder with image files, folder can be insist
                                    subfolders with images templates
@@ -193,95 +181,31 @@ class CheckImages:
         :param min_threshold:      float
         :param precision:          float
         """
-        self.__path_to_tmpls = self.__check_templates(path_to_tmpls)   # tuple, 2 items: 0 - str, path to one image
+        self.__tmpls = tmpls           # tuple, 2 items: 0 - str, path to one image
                                                                        # file or folder with image files, folder
                                                                        # 1 - str, filename if the input is one file,
                                                                        # else - empty string
-        self.__screen_img_list = self.__check_screen_img(screen_img)
-        self.__min_threshold = self.__check_threshold(min_threshold, 'min')
-        self.__precision = self.__check_precision(precision)
+        self.__screen_imgs = screen_imgs
+        self.__min_threshold = min_threshold
+        self.__precision = precision
         self.__output_preparing = None
-        self.__screen_index = 0
         self.__height = None
         self.__width = None
-    
-    def __check_templates(self, path: str) -> (str, str):
-        if os.path.isdir(path):
-            return path, ''
-
-        elif os.path.isfile(path):
-            file_extension = path.split('.')[-1].lower()
-            if file_extension in self.__extension_list:
-                p = os.path.split(path)
-                return p[0], p[1]
-            else:
-                error = f'File {path} not in proper format ({self.__extension_list})'
-                raise IOError(error)
-
-        else:
-            error = f'{path} not found'
-            raise IOError(error)
-
-    def __check_screen_img(self, screen_img: str) -> [str, ]:
-        img_list = screen_img.split(' * ')
-        checked_img_list = []
-        for img in img_list:
-            if os.path.isfile(img):
-                extension = img.split('.')[-1].lower()
-                if extension in self.__extension_list:
-                    checked_img_list.append(img)
-
-        if not checked_img_list:
-            error = f'Any compatible image(-s) in {img_list} not found'
-            raise IOError(error)
-
-        return checked_img_list
-
-    def __check_threshold(self, thr: float, type_: str) -> float:
-        if not isinstance(thr, float):
-            if type_ == 'min':
-                error = f'min_threshold={thr} but it should be a number'
-                raise TypeError(error)
-
-        if 0.1 > thr > 1:
-            if type_ == 'min':
-                error = f'min_threshold={thr} but have to be in range 0.1 ... 1.0'
-                raise ValueError(error)
-
-        return thr
-
-    def __check_precision(self, prec: float) -> float:
-        if not isinstance(prec, float) or not 0 < prec < 1:
-            error = f'precision={prec} but it should be a number in range 0 ... 1'
-            raise TypeError(error)
-
-        return prec
 
     def __round_threshold(self, thr: float) -> float:
         multiplier = 10 ** (len(str(self.__precision)) - 2)
         thr = int(thr * multiplier)
         return thr / multiplier
 
-    def __find_all_template_files(self):
-        if len(self.__path_to_tmpls[1]) == 0:
-            for root, dirs, images in os.walk(self.__path_to_tmpls[0]):
-                prepared_images = []
-                for img in images:
-                    if img.split('.')[-1] in self.__class__.__extension_list:
-                        prepared_images.append(img)
-                self.__class__.__tmpls_dict[root] = prepared_images
-
-        else:
-            self.__class__.__tmpls_dict[self.__path_to_tmpls[0]] = [self.__path_to_tmpls[1]]
-
     def __any_img_to_grayscale(self, path: str) -> numpy.ndarray:
         img_rgb = cv2.imread(path)
         return cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-    def __find_one_image(self, img: str) -> numpy.ndarray:
+    def __find_one_image(self, img: str, scr_img: numpy.ndarray) -> numpy.ndarray:
         img = self.__any_img_to_grayscale(img)
         self.__height, self.__width = img.shape
-        return cv2.matchTemplate(img, self.__class__.__scr_img_gray_list[self.__screen_index], cv2.TM_CCOEFF_NORMED)
+
+        return cv2.matchTemplate(img, scr_img, cv2.TM_CCOEFF_NORMED)
 
     def __filter_near_points(self, found_tmpl_dict: dict) -> list:
         prev_pt = None
@@ -315,21 +239,19 @@ class CheckImages:
 
     def __find_thresholds_for_all_images(self):
         with open('thresholds.txt', 'at', encoding='utf-8') as f:
-            header_info = self.__output_preparing.print_header(self.__screen_img_list[self.__screen_index])
-            f.writelines(header_info[0])    # ------------------------------------------------------
-            f.writelines(header_info[1])    # |        FOLDER - D:\Python\Check Images\tf          |
-            f.writelines(header_info[2])    # |   SCREENSHOT - D:\Python\Check Images\i1_2.png     |
-            f.writelines(header_info[3])    # |   PRECISION - 0.0001   ///   2023-01-05 16:23:44   |
-            f.writelines(header_info[0])    # ------------------------------------------------------
-            f.writelines(header_info[4])    # IMAGE           |COUNT   |THRESHOLD   |X       |Y
-            f.writelines(header_info[0])    # ------------------------------------------------------
+            for scr_path in self.__screen_imgs:
+                scr_img_gray = self.__any_img_to_grayscale(scr_path)
+                header_info = self.__output_preparing.print_header(scr_path)
+                f.writelines(header_info[0])    # ------------------------------------------------------
+                f.writelines(header_info[1])    # |   SCREENSHOT - D:\Python\Check Images\i1_2.png     |
+                f.writelines(header_info[2])    # |   PRECISION - 0.0001   ///   2023-01-05 16:23:44   |
+                f.writelines(header_info[0])    # ------------------------------------------------------
+                f.writelines(header_info[3])    # IMAGE           |COUNT   |THRESHOLD   |X       |Y
+                f.writelines(header_info[0])    # ------------------------------------------------------
 
-            for tmpl_path, tmpl_names_list in self.__class__.__tmpls_dict.items():
-                for tmpl_name in tmpl_names_list:
-                    found_tmpl_dict = dict()
-
-                    tmpl_to_find = tmpl_path + os.sep + tmpl_name
-                    searching = self.__find_one_image(tmpl_to_find)
+                found_tmpl_dict = dict()
+                for tmpl_path in self.__tmpls:
+                    searching = self.__find_one_image(tmpl_path, scr_img_gray)
                     location = numpy.where(searching >= self.__min_threshold)
                     for pt in zip(*location[::-1]):
                         if found_tmpl_dict.get(pt) is None:
@@ -346,36 +268,40 @@ class CheckImages:
                                 tmpl_to_find = ''
                                 num_tmpls_found = ''
 
-                            one_entry_to_output = OutputInfo(self.__path_to_tmpls[0], threshold, self.__precision,
+                            one_entry_to_output = OutputInfo(os.path.basename(tmpl_path),
+                                                             threshold,
+                                                             self.__precision,
                                                              self.__output_preparing.img_indent,
                                                              self.__output_preparing.count_indent,
                                                              self.__output_preparing.threshold_indent,
                                                              self.__output_preparing.coord_indent,
-                                                             tmpl_to_find, num_tmpls_found, x, y)
+                                                             tmpl_to_find,
+                                                             num_tmpls_found,
+                                                             x,
+                                                             y)
                             f.writelines(
                                 one_entry_to_output.print_one_found_entry())  # exit.webp    |  1     |0.8136      |994     |2
 
                             iteration += 1
                     else:
-                        one_entry_to_output = OutputInfo(self.__path_to_tmpls[0], 0, self.__precision,
+                        one_entry_to_output = OutputInfo(os.path.basename(tmpl_path),
+                                                         0,
+                                                         self.__precision,
                                                          self.__output_preparing.img_indent,
                                                          self.__output_preparing.count_indent,
                                                          self.__output_preparing.threshold_indent,
                                                          self.__output_preparing.coord_indent,
-                                                         tmpl_to_find, count=0, x=None, y=None)
+                                                         tmpl_to_find,
+                                                         count=0,
+                                                         x=None,
+                                                         y=None)
                         f.writelines(one_entry_to_output.print_one_not_found_entry())  # fg_win.webp    |  0     |Not found   |None    |None
 
-            f.writelines('\n\n')
+                f.writelines('\n\n')
 
     def run(self):
-        for img in self.__screen_img_list:
-            self.__class__.__scr_img_gray_list.append(self.__any_img_to_grayscale(img))
-        self.__find_all_template_files()
-        self.__output_preparing = InitServiceOutputInfo(self.__class__.__tmpls_dict, self.__path_to_tmpls,
-                                                        self.__precision)
-        for _ in range(len(self.__scr_img_gray_list)):
-            self.__find_thresholds_for_all_images()
-            self.__screen_index += 1
+        self.__output_preparing = InitServiceOutputInfo(self.__tmpls, self.__precision)
+        self.__find_thresholds_for_all_images()
 
 
 def main():
